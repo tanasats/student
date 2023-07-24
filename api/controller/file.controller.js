694d0bc9ca72c7bd6e9ae9abae8674d5e77e3899
tanasats <tanasat.s@msu.ac.th>
2023-07-24 17:21:39 +0700

    checkpoint1

--- api/controller/file.controller.js
+++ api/controller/file.controller.js
@@ -63,7 +63,6 @@ const getListFiles = (req, res) => {
 const download = (req, res) => {
   const fileName = req.params.name;
   const directoryPath = __basedir + "/uploads/";
-
   res.download(directoryPath + fileName, fileName, (err) => {
     if (err) {
       res.status(500).send({
