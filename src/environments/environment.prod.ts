405d62e7d576ebbf3abb8824f757a389009d1c9c
tanasats <tanasat.s@msu.ac.th>
2023-08-14 22:04:45 +0700

    v1.0.1

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,8 +2,8 @@
 export const environment = {
     production: true,
     environmentName: "production",
-    version:'1.0.0',
-    builddate:'2023-08-09T05:40:14Z',
+    version:'1.0.1',
+    builddate:'2023-08-14T22:03:11Z',
     endpoint: "https://activity.msu.ac.th/api/sas",
     fileuri: "https://activity.msu.ac.th/api/sas/",
     msuauthapi: "https://data.msu.ac.th/api/v1.1/auth",
